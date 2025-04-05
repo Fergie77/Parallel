b8c8fdfb45918c5b8e6d9d7c36dd34a8c59e163a
Fergie77 <fergie2@live.co.uk>
2025-04-05 10:15:38 +0100

    u

--- src/Animations/NumberCounterInitialLoad.js
+++ src/Animations/NumberCounterInitialLoad.js
@@ -3,17 +3,29 @@ import { Odometer } from 'odometer_countup'
 
 export const numberCounterInitialLoad = () => {
   return new Promise((resolve, reject) => {
+    const lastVisit = localStorage.getItem('lastVisit')
+    const today = new Date().toDateString()
+
+    // Determine animation duration based on previous visits
+    const animationDuration = lastVisit === today ? 0.5 : 2
+
     const countUp = new CountUp('initial-odometer', 100, {
       startVal: 0,
-      duration: 3, // Duration in seconds
+      duration: animationDuration, // Adjusted duration based on previous visits
       useGrouping: false,
       decimalPlaces: 0, // No decimal places
       formattingFn: (n) => {
         return n.toString().padStart(3, '0')
       },
-      plugin: new Odometer({ duration: 1, lastDigitDelay: 0 }),
+      plugin: new Odometer({
+        duration: animationDuration / 3,
+        lastDigitDelay: 0,
+      }), // Adjusted Odometer duration proportionally
       onComplete: () => {},
     })
+
+    // Store today's date as the last visit
+    localStorage.setItem('lastVisit', today)
 
     if (!countUp.error) {
       countUp.start(() => {
